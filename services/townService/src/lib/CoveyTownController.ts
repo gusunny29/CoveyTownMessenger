71ed2e6164907919a9bc3ddb01c99dae909ec782
rajp33 <rpatel3318@gmail.com>
2022-04-24 13:38:44 -0400

    Rebase Branch onto Main Branch

--- services/townService/src/lib/CoveyTownController.ts
+++ services/townService/src/lib/CoveyTownController.ts
@@ -395,12 +395,12 @@ export default class CoveyTownController {
   }
 
   /**
- * Adds a player's id to the list of blocked player id's for a player
- *
- * @param blockingPlayerID represents the unique ID of blocking player
- * @param blockedPlayerID represents the unique ID of the player to block 
- * @returns whether the player was successfully blocked
- */
+   * Adds a player's id to the list of blocked player id's for a player
+   *
+   * @param blockingPlayerID represents the unique ID of blocking player
+   * @param blockedPlayerID represents the unique ID of the player to block
+   * @returns whether the player was successfully blocked
+   */
   blockPlayer(blockingPlayerID: string, blockedPlayerID: string): boolean {
     const blockingPlayer = this.players.find(p => p.id === blockingPlayerID);
     const blockedPlayer = this.players.find(p => p.id === blockedPlayerID);
